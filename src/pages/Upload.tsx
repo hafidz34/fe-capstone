0ad84ca7cb0ddce30f56dd9227e0d066b140283d
allyadzhani <115388817+allyadzhani@users.noreply.github.com>
2025-05-25 15:48:12 +0700

    Update and rename Contact.tsx to Upload.tsx

---
+++ src/pages/Upload.tsx
@@ -0,0 +1,15 @@
+import FileUploadButton from "../components/FileUploadButton";
+
+export default function Upload() {
+  const handleFilesSelected = (files: FileList) => {
+    console.log("Selected files:", files);
+  };
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <FileUploadButton
+      onFilesSelected={handleFilesSelected}
+      uploadUrl="/api/data/upload"
+      />
+    </div>
+  );
+}
