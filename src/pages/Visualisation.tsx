93b61ff514ad78f168cd171a7e31dcaad95558fe
RadenFitzalBintangNW <142723166+Fitznug045@users.noreply.github.com>
2025-05-31 21:58:49 +0700

    Update Visualisation.tsx

--- src/pages/Visualisation.tsx
+++ src/pages/Visualisation.tsx
@@ -1,7 +1,9 @@
+import Excelchart from "../components/Excelchart";
+
 export default function Visualisation() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#F3F4FF]">
-      <h1 className="text-4xl font-bold mb-4">Placeholder 2</h1>
+      <Excelchart />
     </div>
-  )
+  );
 }
